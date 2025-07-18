d92c2cd05c17b6cb3344954a8af36ed59e30c979
LILIK SETIYONO <060101396@intranet.pajak.go.id>
2025-07-19 00:50:36 +0700

    pisahkan tombol google dari loginform.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -82,25 +82,7 @@ export default function LoginForm() {
         )}
       </fieldset>
 
-      <div className="text-center space-y-4">
-        <p className="text-gray-500">or</p>
-        <Button
-          type="button"
-          className="w-full bg-gray-50 text-gray-900 font-medium border border-gray-300 hover:bg-gray-200"
-        >
-          <img src="/assets/google.png" alt="Google Logo" className="w-4 h-4" />
-          Continue with Google
-        </Button>
-        <p className="text-sm text-gray-600">
-          Not a member yet?{" "}
-          <a
-            href="/register"
-            className="text-blue-600 hover:underline font-medium"
-          >
-            Join the Club
-          </a>
-        </p>
-      </div>
+     
     </form>
   );
 }
